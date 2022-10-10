a36ea71497fe9d179c9f104bba101f69299aa253
gaabscps <gaabscps@gmail.com>
2022-10-10 10:37:07 -0300

    :construction: aula 1

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Navbar } from "./components/Navbar";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <div>hello world</div>
   </React.StrictMode>
 );
